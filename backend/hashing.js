4cb6f51764dd107a19ec23dc8d467ab499a72a6b
Debojyoti <sahadebojyoti955@gmail.com>
2025-07-07 13:03:28 +0530

    updated

--- backend/hashing.js
+++ backend/hashing.js
@@ -2,5 +2,5 @@ import bcrypt from 'bcryptjs';
 // const hashed = await bcrypt.hash('Admin@123', 10); // $2b$10$Ko8tsMfxd4crVn0dE6CZ7eyl7sVfaz2ewnGKinnxh4Dxl/IadydKK
 
 
-const hashed = bcrypt.hashSync('User@001', 10); // $2b$10$pLNmEHLq2cqmaF.rt4HP2OcKRyW5Ab3i1cfSIGy9JkRqa5Bx0mhMG
+const hashed = bcrypt.hashSync('User@0012', 10); // $2b$10$pLNmEHLq2cqmaF.rt4HP2OcKRyW5Ab3i1cfSIGy9JkRqa5Bx0mhMG
 console.log(hashed);
